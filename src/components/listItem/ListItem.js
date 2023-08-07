b64821478fc4ef5da2031b61f6af6b3bfe79d11c
RishiJhawar14 <96866023+RishiJhawar14@users.noreply.github.com>
2023-08-07 19:59:00 +0530

    Revert "Updated Api Calls"
    This reverts commit 40f415b4c4b56052f911c109d7f4620119d73dff.

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -17,7 +17,7 @@ export default function ListItem({ index, item }) {
   useEffect(() => {
     const getMovie = async () => {
       try {
-        const res = await axios.get("https://netflix-clone-api-5cn1.onrender.com/api/movies/find/" + item, {
+        const res = await axios.get("/movies/find/" + item, {
           headers: {
             token:
               "Bearer " + JSON.parse(localStorage.getItem("user")).accessToken,
